e7110add3e5e631c774e88fc47e1dba94a3a15c9
Chris Hackett <chrisjhackett@gmail.com>
2016-03-17 16:21:43 -0400

    adding copy stuff

--- consutil1/interpret1.cs
+++ consutil1/interpret1.cs
@@ -1,34 +1,94 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace consutil1 {
-    class interpret1 {
+
+    delegate void AccumInfo(string val);
+
+    class Interpret1 {
         static void Main(string[] args) {
-            Operator1 oper = new Operator1();
-            oper.Main(args);
+            Operator1 oper = new Operator1(myAccumInfo);
+            int resVal = oper.Main(args);
+            Console.WriteLine("\n Interpret1.log begin (result={0}) ",resVal);
+            foreach (var var in errInfo) {
+                Console.WriteLine(var);
+            }
+            Console.WriteLine("\n Interpret1.log end");
         }
+
+        static void myAccumInfo(string val) {
+            lock(errInfo) {
+                errInfo.Add(val);
+            }
+        }
+
+        static List<string>  errInfo = new List<string>();
     }
 
     class Operator1 {
+        private AccumInfo myLog = null;
+        private int lastErr = 0;
+        public Operator1(AccumInfo errorLog) {
+            myLog = errorLog ?? myAccumInfo;
+        }
+
+        List<string> errInfo = new List<string>();
+        void myAccumInfo(string val) {
+            lock (errInfo) {
+                errInfo.Add(val);
+            }
+        }
 
         public int Main(string[] args) {
             int rVal = 0;
             StringDictionary opts;
             StringDictionary pars;
             int argsErr = getOpts(args, out opts, out pars);
-            try {
-                switch (args[0]) {
-                    case "help": Console.WriteLine("no help here");
-                        break;
+            Console.WriteLine("Operator1.Main.opts returns: {0:X}({0})", argsErr);
+
+            foreach (String nam in opts.Keys) {
+                try {
+                    switch (nam) {
+                        case "help":
+                            Console.WriteLine("no help here");
+                            break;
+                    }
+                }
+                catch (Exception exc) {
+                    rVal = exc.HResult;
+                    string msg = errMsg(exc, "Operator1.Main.opts");
+                    Console.WriteLine(msg);
+                    myLog(msg);
                 }
             }
-            catch (Exception exc) {
-                rVal = exc.HResult;
-                Console.WriteLine(errMsg(exc,"Operator1.Main"));
+
+
+            foreach (String nam in pars.Keys) {
+                try {
+                    switch (nam) {
+                        case "oper":
+                            switch (pars[nam]) {
+                                case "copy": {
+                                        rVal = OperCopy(opts, pars);
+                                    }
+                                    break;
+
+                            }
+                            break;
+                    }
+                }
+                catch (Exception exc) {
+                    rVal = exc.HResult;
+                    string msg = errMsg(exc, "Operator1.Main.pars");
+                    Console.WriteLine(msg);
+                    myLog(msg);
+                }
             }
             return rVal;
         }
@@ -36,8 +96,6 @@ namespace consutil1 {
         string errMsg(Exception exc, string where = "[not specified]") {
             return String.Format("Exception in '{3}': {0,8:X}({0}) {1}:{2}", exc.HResult, exc.Message, (exc.InnerException != null) ? exc.InnerException.Message : "[no inner]", where);
         }
-
-
 
         int getOpts(IEnumerable<string> args, out StringDictionary opts, out StringDictionary pars) {
             int rVal = 0;
@@ -65,12 +123,15 @@ namespace consutil1 {
                         targ.Add(nam, val);
                     }
                     if (isOpt && nam.Equals("ThRoW")) {
+                        int iv = int.Parse(val);
                         throw new Exception(val);
                     }
                 }
                 catch (Exception exc) {
-                    if (rVal == 0) rVal = exc.GetHashCode();
-                    Console.WriteLine(errMsg(exc, stgVal));
+                    if (rVal == 0) rVal = exc.HResult;
+                    string msg = errMsg(exc, "Operator1.Main.opts");
+                    //Console.WriteLine(msg);
+                    myLog(msg);
                 }
             }
 
@@ -85,6 +146,39 @@ namespace consutil1 {
             }
 
             return rVal;
+        }
+
+        int OperCopy(StringDictionary opts, StringDictionary pars) {
+            string src = pars["src"];
+            string des = pars["des"];
+            if (src == null) DoLogError("need [src] directory ");
+            if (des == null) DoLogError("need [des] directory ");
+            if (src.Equals(des)) DoLogError("src can not equal des ");
+            if (lastErr != 0) return lastErr;
+
+            if (!Directory.Exists(src)) DoLogError("[src] directory does not exist");
+            if (Directory.Exists(src)) DoLogError("[des] directory exists already",0);
+            if (lastErr != 0) return lastErr;
+
+            string forceCopyRoot = @"g:\junk\des";
+
+            if (!(src.IndexOf(forceCopyRoot) == 0)) {
+                DoLogError("currently forcing [des] foot to :"+forceCopyRoot, 2);
+            }
+
+            if (lastErr != 0) return lastErr;
+
+
+
+
+
+            return lastErr;
+        }
+
+        int DoLogError(string errMsg, int errVal = -1) {
+            if (errVal != 0) lastErr = errVal;
+            myLog(errMsg);
+            return lastErr;
         }
 
 
